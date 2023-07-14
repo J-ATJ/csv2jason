7b3aa4bf7415315fed649679eafeb3662f74e048
J-ATJ <98430219+J-ATJ@users.noreply.github.com>
2023-07-13 21:16:07 -0600

    no comments

--- csv2json.py
+++ csv2json.py
@@ -10,5 +10,5 @@ def csv_to_json(csv_file, json_file):
     with open(json_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-# Uso
+
 csv_to_json('input.csv', 'output.json')
